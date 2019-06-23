b3d3eca1acd2968aca1f7500dd1a14801a3c233a
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 23:56:47 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -6,29 +6,29 @@ const superagent = require("superagent")
 module.exports.run = async (client, message, args) => {
     let msg = await message.channel.send("Generating...")
 
-    let {image} = await superagent
-    .get(`https://api-to.get-a.life/meme`)
-    console.log(image.file)
-    if(!{image}) return message.channel.send("I broke! Try again.")
+    let {body} = await superagent
+    .get(`http://aws.random.cat/meow`)
+    console.log(body.file)
+    // if(!{image}) return message.channel.send("I broke! Try again.")
 
-        let mEmbed = new Discord.RichEmbed()
-        .setColor(colours.cyan)
-        .setAuthor(`TestBot MEMES!`, message.guild.iconURL)
-        .setImage(image)
-        .setTimestamp()
-        .setFooter(`TEST BOT`, client.user.displayAvatarURL)
+        //let mEmbed = new Discord.RichEmbed()
+        //.setColor(colours.cyan)
+        //.setAuthor(`TestBot MEMES!`, message.guild.iconURL)
+        //.setImage(image)
+        //.setTimestamp()
+       // .setFooter(`TEST BOT`, client.user.displayAvatarURL)
 
-        message.channel.send({embed: mEmbed})
+       // message.channel.send({embed: mEmbed})
 
-        msg.delete();
+      //  msg.delete();
 }
 
 
-module.exports.config = {
-    name: "meme",
-    noalias: "No Aliases",
-    description: "Sends a meme from a website!",
-    usage: "!meme",
-    accessableby: "Members",
-    aliases: []
-}
+//module.exports.config = {
+   // name: "meme",
+   // noalias: "No Aliases",
+    //description: "Sends a meme from a website!",
+   // usage: "!meme",
+   // accessableby: "Members",
+   // aliases: []
+//}
