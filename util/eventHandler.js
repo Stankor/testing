f79057e6802ee4e64575ea9c1aac372b0fb5ff79
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 22:42:26 -0400

    Update eventHandler.js

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -1,6 +1,6 @@
 const reqEvent = (event) => require(`../events/${event}`)
 
-module.exports = bot => {
+module.exports = client => {
     bot.on("ready", function() {reqEvent("ready") (client) });
     bot.on("reconnecting", () => reqEvent("reconnecting") (client))
     bot.on("disconnect", () => reqEvent("disconnect") (client))
