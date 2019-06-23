34a6d17f2597cfb0e5f2723d3837006b376fed4f
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 22:50:38 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+
+module.exports = client => {
+     console.log(`${bot.user.username} está online`)
+    // bot.user.setActivity("Stankor Bot 2.0", {type: "STREAMING", url:"a"});
+
+    let statuses = [
+        `${bot.guilds.size} servers!`,
+        "!help",
+        `over ${bot.users.size} users!`
+    ]
+
+    setInterval(function() {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        bot.user.setActivity(status, {type: "WATCHING"});
+
+    }, 5000)
+
+}
