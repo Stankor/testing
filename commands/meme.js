7289a981e9182af60ccacda279a6a46bf4597025
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-23 00:09:51 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -4,31 +4,9 @@ const superagent = require("superagent")
 
 
 module.exports.run = async (client, message, args) => {
-    let msg = await message.channel.send("Generating...")
+    let msg = await message.channel.send("Carregando...")
 
     let {body} = await superagent
-    .get(`http://aws.random.cat/meow`)
-    console.log(body.file)
-    if(!{body}) return message.channel.send("I broke! Try again.")
-
-        let mEmbed = new Discord.RichEmbed()
-        .setColor(0xffffff)
-        .setAuthor(`TestBot MEMES!`, message.guild.iconURL)
-        .setImage(body.file)
-        .setTimestamp()
-        .setFooter(`TEST BOT`, client.user.displayAvatarURL)
-
-       message.channel.send({embed: mEmbed})
-
-      msg.delete();
-}
-
-
-module.exports.config = {
-   name: "meme",
-   noalias: "No Aliases",
-   description: "Sends a meme from a website!",
-   usage: "!meme",
-   accessableby: "Members",
-   aliases: []
-}
+    .get(`https://apis.duncte123.me/meme`)
+    console.log(image.file)
+   
