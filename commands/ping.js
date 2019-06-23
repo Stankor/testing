bb9440175f1ada2fab2b92801f2cfbbc516462b6
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-23 01:20:49 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,12 @@ const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
 
-    message.channel.send("Pinging...").then(m => {
+    message.channel.send(":satellite_orbital: Verificando latência...").then(m => {
         let ping = m.createdTimestamp - message.createdTimestamp
         let choices = ["Esse é realmente meu ping?", "Está tudo bem? Eu não consigo ver", "Eu espero que isso não seja ruim"]
         let response = choices[Math.floor(Math.random() * choices.length)]
 
-        m.edit(`:envelope_with_arrow: ${response}: :satellite: Latência do bot: \`${ping}\`, :satellite: Latência da API: \`${Math.round(client.ping)}\``)
+        m.edit(`:satellite: Latência do bot: \`${ping}\`. | :satellite: Latência da API: \`${Math.round(client.ping)}\`.`)
     })
 
 }
@@ -20,6 +20,6 @@ module.exports.config = {
     name: "ping",
     description: "PONG! Exibe a latência do bot e da api",
     usage: "!ping",
-    accessableby: "Members",
-    aliases: ["latency"]
+    accessableby: "Membros",
+    aliases: ["latency", "latencia", "latência"]
 }
