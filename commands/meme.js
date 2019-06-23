73334358ee47aba2e3f6698ab51d09375660aa22
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-22 23:37:42 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,33 +1,36 @@
 // Comando de memes.
-const Discord = require("discord.js");
-const superagent = require("superagent");
+const Discord = require("discord.js")
+const botconfig = require("../botconfig.json");
+const colours = require("../colours.json");
+const superagent = require("superagent")
 
-module.exports.run = (client, message, args) => {
 
-    let msg = message.channel.send('Carregando...')
+module.exports.run = async (bot, message, args) => {
+    let msg = await message.channel.send("Generating...")
 
-    let {body} = superagent
-    .get(`https://apis.duncte123.me/meme`)
-    // console.log(body.file)
-    if(!{body}) return message.channel.send('Eu buguei! Tente novamente.')
+    let {url} = await superagent
+    .get(`https://api-to.get-a.life/meme`)
+    //console.log(body.file)
+    if(!{url}) return message.channel.send("I broke! Try again.")
 
         let mEmbed = new Discord.RichEmbed()
-            .setColor(0xffffff)
-            .setAuthor('Memes', message.guild.iconURL)
-            .setImage({body}.url)
-            .setTimestamp()
-            .setFooter('Sou um humano disfarçado', bot.user.avatarURL)
+        .setColor(colours.cyan)
+        .setAuthor(`TestBot MEMES!`, message.guild.iconURL)
+        .setImage(url)
+        .setTimestamp()
+        .setFooter(`TEST BOT`, bot.user.displayAvatarURL)
+
         message.channel.send({embed: mEmbed})
 
         msg.delete();
-
 }
+
 
 module.exports.config = {
     name: "meme",
     noalias: "No Aliases",
-    description: "Manda um meme de um site.",
+    description: "Sends a meme from a website!",
     usage: "!meme",
-    accessableby: "membros",
+    accessableby: "Members",
     aliases: []
 }
