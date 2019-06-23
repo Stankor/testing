19da63e0e0fd1c5b9e1f412f9f8f7335f6ddbb2f
Stankor <52055009+Stankor@users.noreply.github.com>
2019-06-23 00:42:33 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,4 +1,4 @@
-// Comando de memes.
+// Comando de gatos
 const Discord = require("discord.js")
 const superagent = require("superagent")
 
@@ -7,16 +7,26 @@ module.exports.run = async (client, message, args) => {
     let msg = await message.channel.send("Carregando...")
 
     let {body} = await superagent
-    .get(`https://apis.duncte123.me/meme`)
-    console.log(body.data.title)
+    .get(`http://aws.random.cat/meow`)
+    //console.log(body.file)
+    if(!{body}) return message.channel.send("Eu buguei! Tente novamente.")
+
+        let mEmbed = new Discord.RichEmbed()
+        .setColor(0xffffff)
+        .setAuthor(Gatito, message.guild.iconURL)
+        .setImage(body.file)
+        .setTimestamp()
+        .setFooter(Sou um humano disfarçado, client.user.displayAvatarURL)
+
+       message.channel.send({embed: mEmbed})
+
+      msg.delete();
 }
 
-
 module.exports.config = {
-   name: "meme",
-   noalias: "No Aliases",
-   description: "Sends a meme from a website!",
-   usage: "!meme",
-   accessableby: "Members",
-   aliases: []
+    name: "cat",
+    description: "Envia uma imagem de gato.",
+    usage: ";cat",
+    accessableby: "Membros",
+    aliases: ["cat", "gato", "gatito"]
 }
